70e2eef9ad541584d3caf56148409d99491ae7bd
JIYA <jiya@JIYAs-MacBook-Air.local>
2020-07-29 02:18:56 +0530

    commit

--- cnn.py
+++ cnn.py
@@ -6,8 +6,6 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 trainDataGen = ImageDataGenerator(rescale = 1./255, zoom_range = 0.2, shear_range = 0.2, horizontal_flip = True)
 testDataGen = ImageDataGenerator(rescale = 1./255, zoom_range = 0.2, shear_range = 0.2, horizontal_flip = True)
 
-path='./Dataset/Garbage classification/Garbage classification'
-
 trainX = trainDataGen.flow_from_directory('./Dataset/Garbage classification', target_size=(64,64), batch_size=32, class_mode='categorical')
 testX = testDataGen.flow_from_directory('./Dataset/Garbage classification', target_size=(64,64), batch_size=32, class_mode='categorical')
 
